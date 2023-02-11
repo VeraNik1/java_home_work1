4529d33835cbfe10c1494b6d3113704b09611ec4
Vera <solnce288888@gmail.com>
2023-02-11 20:45:10 +0300

    task02 edited

--- task02.java
+++ task02.java
@@ -1,38 +1,15 @@
-import java.util.Scanner;
-/*Вывести все простые числа от 1 до 1000 */
+/* Вывести все простые числа от 1 до 1000 */
 
 public class task02 {
     public static void main(String[] args) {
-        while (true) {
-            try {
-                System.out.println("Введите натуральное число больше 1 ");
-                Scanner inter = new Scanner(System.in);
-                int number = inter.nextInt();
-                if (number > 2) {
-                    System.out.printf("Простые числа от 1 до %d >>> 2 ", number); // число 1 не является ни простым, ни составным
-                    for (int i = 3; i <= number; i += 2) {
-                        if (checkPrimeNumber(i)) {
-                            System.out.printf("%d ", i);
-                        }
-                    }
-                    break;
-                } else if (number == 2) {
-                    System.out.printf("Простые числа от 1 до %d >>> 2 ", number);
-                    
-                    break;
-                }
+       int number = 1000;
+       System.out.printf("Простые числа от 1 до %d >>> 2 ", number); // число 1 не является ни простым, ни составным
+       for(int i = 3; i <= number; i += 2) 
+          {if (checkPrimeNumber(i)) {
+              System.out.printf("%d ", i);
+          }
+                        }}
 
-                else {
-                    System.out.println("Введено число меньше 1, попробуйте еще раз!");
-                }
-            }
-
-            catch (Exception e) {
-                System.out.println("введено не натуральное число, попробуйте еще раз!");
-            }
-
-        }
-    }
     // проверка является ли число простым
     static boolean checkPrimeNumber(int num) {
         if (1 < num && num <= 3) {
@@ -40,12 +17,8 @@ public class task02 {
         } else {
             for (int i = 2; i <= num / 2 + 1; i++) {
                 if (num % i == 0) {
-                    return false;
-                }
-            }
+                    return false;}}
 
         }
-        return true;
+        return true;}
     }
-
-}
