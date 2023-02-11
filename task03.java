e3520428199c39798ad277d9beab1edf61351745
Vera <solnce288888@gmail.com>
2023-02-11 20:27:19 +0300

    task03 finish

--- task03.java
+++ task03.java
@@ -1,36 +1,44 @@
 import java.util.Scanner;
 
 public class task03 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         while (true) {
             try {
-
-                Scanner inter = new Scanner(System.in);
-                System.out.println("Введите первое число" );
-                double number1 = inter.nextDouble();
-                System.out.println("Введите второе число" );
-                double number2 = inter.nextDouble();
-                System.out.println("Введите одно из следующих действий '/'- деление;\n '*' - умножение;\n' '+' - сумма; \n '-' - разность; \n '^' - возведение в степень");
-                String act = inter.next();
-                } 
-
-            catch (Exception e) {
-                System.out.println("Некорректный ввод, попробуйте еще раз!");
-            }
-        }
-
-    }
-
-    static double calc(int n1, int n2, char act) {
-        if(act == '+'){ return n1 + n2;}
+                Scanner inter = new Scanner (System.in);
+                System.out.println ("Введите первое число" );
+                double num1 = inter.nextDouble();
+                System.out.println ("Введите второе число" );
+                double num2 = inter.nextDouble();
+                System.out.println ("Введите одно из следующих действий:\n '/'- деление;\n '*' - умножение;\n '+' - сумма; \n '-' - разность; \n '^' - возведение в степень");
+                char op = inter.next().charAt(0); 
+                double ans;
+                switch(op) {
+                    case '+': ans = num1 + num2;
+                    break;
+                    case '-': ans = num1 - num2;
+                    break;
+                    case '*': ans = num1 * num2;
+                    break;
+                    case '/': {
+                    if(num2 == 0){
+                        System.out.printf("На ноль делить нельзя!");
+                        return;
+                    }
+                    ans = num1 / num2;
+                    break;}
+                    case '^': ans = Math.pow(num1 , num2);
+                    break;
+                    default:  System.out.printf("Ошибка, введен некорректный оператор");
+                    return;}
+                
+                System.out.printf("%.0f %s %.0f = %s", num1, op, num2, ans);
+                break;}
     
-        else if(act == '-'){return n1 - n2;}
-        else if(act == '/' && n2 != 0){ return n1/n2;}
-        else if(act == '+'){return n1+n2;}
-        else if(act == '^'){return n1^n2;}
-        else if(act == '*'){return n1*n2;}
-    }
-
-}
-
+            catch (Exception e) 
+                      {System.out.println("Некорректный ввод чисел, попробуйте еще раз!");
+                       }
+                      } 
+                      }
+   }
+                     
 
